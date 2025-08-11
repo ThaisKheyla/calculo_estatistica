27789fb1603163d3774694609d20065dddac1562
ThaisKheyla <kheyla.paucara@sptech.school>
2025-08-11 12:37:12 -0300

    Operações matemáticas simples com números decimais ou tipo ponto flutuante

---
+++ operacoes_1.py
@@ -0,0 +1,19 @@
+#
+print ('\n'*50)
+
+a1=6,5
+b1=2,5
+# Vamos fazer operações mais elaboradas
+# usando as 4 operações simples
+soma = a1+b1
+subtracao =a1-b1
+multiplicacao = a1*b1
+divisao =a1/b1
+
+print ("Vamos mostrar todos os resultados")
+print ("a1=", a1)
+print ("b1=", b1)
+print ("a1+b1=",soma)
+print ("a1-b1=",subtracao)
+print ("a*b=",multiplicacao)
+print ("a/b=",divisao)
