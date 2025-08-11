20b7f1f7f30a193b8aa20234f466dd3b7e3cce9b
ThaisKheyla <kheyla.paucara@sptech.school>
2025-08-11 13:28:55 -0300

    Atividade 1

---
+++ exercicio1.py
@@ -0,0 +1,18 @@
+import math
+print('\n' * 50)
+
+a = 2
+
+#calcular potencia
+potencia_ao_quadrado = a**2
+potencia_ao_cubo = a**3
+potencia_a_quarta = a**4
+
+import math
+print('\n' * 50)
+
+a = 2
+
+print(f"Potência ao quadrado: {a**2}\n"
+      f"Potência ao cubo: {a**3}\n"
+      f"Potência à quarta: {a**4}")
