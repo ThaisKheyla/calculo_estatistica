837cc6418437ad3ccfd5abb43746ed28ff025ef0
ThaisKheyla <kheyla.paucara@sptech.school>
2025-08-11 12:33:23 -0300

    Operações matemáticas simples

---
+++ Operacoes.py
@@ -0,0 +1,21 @@
+# Operações matemáticas
+print("\n" * 100)
+
+# Atribuir valores inteiros a duas variáveis
+a = 6
+b = 3
+
+# Operações mais elaboradas
+# usando as 4 operaçõe/s simples
+soma = a+b
+subtracao =a-b
+multiplicacao = a*b
+divisao =a/b
+
+print ("Vamos mostrar todos os resultados")
+print ("a=", a)
+print ("b=", b)
+print ("a+b=",soma)
+print ("a-b=",subtracao)
+print ("a*b=",multiplicacao)
+print ("a/b=",divisao)
