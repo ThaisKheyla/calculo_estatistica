4ff8c143f9c18fec4c5922bd0c48b85fee4b7baf
ThaisKheyla <kheyla.paucara@sptech.school>
2025-08-11 13:30:27 -0300

    Atividade 8

---
+++ exercicio8.py
@@ -0,0 +1,12 @@
+import math
+print('\n' * 50)
+a = 1
+
+a = float(input("Insira um número: "))
+exe1 = a ** 0
+exe2 = a ** 1
+b = float(input("Insira o valor do expoente: "))
+exe3 = a ** -b
+
+print(exe1)
+print(exe2)
