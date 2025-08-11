f0995358d2b450bdac2c06ef47f9af03febe9697
ThaisKheyla <kheyla.paucara@sptech.school>
2025-08-11 13:30:02 -0300

    Atividade 6

---
+++ exercicio6.py
@@ -0,0 +1,13 @@
+import math
+print('\n' * 50)
+
+# Arredondar os números a seguir, exibindo sem casas decimais
+x1=1.456
+x2=3.678
+x3=7.5
+
+print(
+    f"\n Arredondado o x1 para 0 casas: {round(x1, 0)}",
+    f"\n Arredondado o x2 para 0 casas: {round(x2, 0)}", 
+    f"\n Arredondado o x3 para 0 casas: {round(x3, 0)}"
+)
