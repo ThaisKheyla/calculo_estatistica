13fbbe2bb39eefb6dc1bb5b793a4017e2738dfe7
ThaisKheyla <kheyla.paucara@sptech.school>
2025-08-11 13:29:26 -0300

    Atividade 3

---
+++ exercicio3.py
@@ -0,0 +1,14 @@
+import math
+print('\n' * 50)
+
+x = 512
+
+raiz_quadrada_de_x = math.pow(x, 2)
+raiz_cubica_de_x = math.pow(x, 3)
+raiz_quarta_de_x = math.pow(x, 4)
+
+print(
+    f"\n Raiz cúbica de X: ", raiz_cubica_de_x, 
+    f"\n Raiz quadrada de X: ", raiz_quadrada_de_x,
+    f"\n Raiz quarta de X: ", raiz_quarta_de_x
+)
